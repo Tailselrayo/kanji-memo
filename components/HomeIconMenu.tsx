721b0665d3c97aa8e1f7ef62ab00a06f75665439
tailselrayo <rodtailselrayo@gmail.com>
2023-07-26 11:30:13 +0200

    feat(app): added an information box

--- components/HomeIconMenu.tsx
+++ components/HomeIconMenu.tsx
@@ -1,4 +1,4 @@
-import { Group, ActionIcon, Modal } from "@mantine/core";
+import { Group, ActionIcon, Modal, Title } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { IconInfoSquareFilled, IconVolume, IconVolumeOff } from "@tabler/icons-react";
 import { InformationBox } from "./InformationBox";
@@ -14,7 +14,16 @@ export function HomeIconMenu(props: HomeIconMenuProps) {
   const iconSize = props.iconSize ? props.iconSize : 50
   return (
     <>
-      <Modal opened={opened} onClose={modalHandlers.close}>
+      <Modal
+        size="xl"
+        opened={opened}
+        onClose={modalHandlers.close}
+        title={<Title ta="center" >Informations</Title>}
+        styles={(theme)=>({
+          content: {backgroundColor: theme.colors.dark[5], color: theme.colors.gray[1]},
+          header: {backgroundColor: "black", color: theme.colors.gray[1]}
+        })}
+      >
         <InformationBox />
       </Modal>
       <Group position="right">
--- components/InformationBox.tsx
+++ components/InformationBox.tsx
@@ -1,39 +1,44 @@
-import { Carousel } from "@mantine/carousel";
-import { Title, Text, Group, List } from "@mantine/core";
+import { Carousel, Embla, useAnimationOffsetEffect } from "@mantine/carousel";
+import { Title, Text, Group, List, Box } from "@mantine/core";
 import Image from "next/image";
+import { useState } from "react";
 
-interface InformationBoxProps { }
+export function InformationBox() {
+    const [embla, setEmbla] = useState<Embla>();
 
-export function InformationBox(props: InformationBoxProps) {
+    useAnimationOffsetEffect(embla, 300)
     return (
         <>
-            <Title ta="center" >Informations</Title>
-            <Carousel 
-                slideSize="100%" 
-                slideGap="xs" 
-                styles={{control:{'&[data-inactive]':{opacity: 0,cursor: 'default'}}}}
+            <Carousel
+                slideSize="100%"
+                slideGap="xs"
+                styles={{ control: { '&[data-inactive]': { opacity: 0, cursor: 'default' } } }}
                 withIndicators
+                getEmblaApi={setEmbla}
             >
-                <Carousel.Slide>
+                <Carousel.Slide p="sm">
                     <Title ta="center" fz="xl">Game presentation</Title>
-                    <Group>
-                        <Image src="/game_illustration.png" alt="ill" width={60} height={100}/>
-                        <List>
-                            <List.Item>Each game will take place on a board, as shown on the side</List.Item>
+                    <Group noWrap p="sm">
+                        <Image src="/clean_game.png" alt="ill" width={300} height={200} />
+                        <List p="sm" c="gray.1" ta="justify">
+                            <List.Item >Each game will take place on a board, as shown on the side</List.Item>
                             <List.Item>Cards on the left have kanji on them, on the right are written their meanings</List.Item>
                             <List.Item>The goal is to match each kanji with its meaning</List.Item>
                             <List.Item>The less tries you take to match all kanji, the more points you will gather</List.Item>
                         </List>
                     </Group>
                 </Carousel.Slide>
-                <Carousel.Slide>
+                <Carousel.Slide p="sm">
                     <Title ta="center" fz="xl">How to play</Title>
-                    <List>
-                        <List.Item>Click on a card to turn it around, be careful as only one card per type can be turned at once</List.Item>
-                        <List.Item>When all cards are correctly matched, the step ends. There are 3 steps per game.</List.Item>
-                        <List.Item>First step will have 4 kanji to match, and 4 more will be added each step</List.Item>
-                        <List.Item>Difficulty corresponds to japanese grading of kanji, and can be setup before or after any game</List.Item>
-                    </List>
+                    <Group noWrap p="sm">
+                        <Image src="/game_illustration.png" alt="ill2" width={150} height={200} />
+                        <List p="sm" c="gray.1" ta="justify">
+                            <List.Item>Click on a card to turn it around, be careful as only one card per type can be turned at once</List.Item>
+                            <List.Item>When all cards are correctly matched, the step ends. There are 3 steps per game.</List.Item>
+                            <List.Item>First step will have 4 kanji to match, and 4 more will be added each step</List.Item>
+                            <List.Item>Difficulty corresponds to japanese grading of kanji, and can be setup before or after any game</List.Item>
+                        </List>
+                    </Group>
                 </Carousel.Slide>
             </Carousel>
         </>
